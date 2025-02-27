58e2db0c6b1029b9d00c36b9df1789f6109d2792
Demir Evren <evren.demir@student.pxl.be>
2025-02-27 15:16:58 +0100

    update

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,8 @@
 const Sequelize = require('sequelize');
 
-// const isDocker = process.env.TEST_MODE === "true" || process.env.DOCKER_ENV === "true";
-
-
 const sequelize = new Sequelize({
     dialect: 'mysql',
-    host: process.env.DBURL || 'localhost',  // ✅ Now using DBURL
-    // host: process.env.DBURL || 'test-mysql',  // ✅ Now using DBURL
+    host: process.env.DBURL || 'localhost', 
     username: process.env.DBUSER || 'root',
     password: process.env.DBPASSWORD || '',
     database: process.env.DBDATABASE || 'todo',
