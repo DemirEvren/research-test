81cf4199eb402acff041177c66148e9eaf382040
Demir Evren <evren.demir@student.pxl.be>
2025-03-01 16:21:28 +0100

    update

--- backend/tests/api.test.js
+++ backend/tests/api.test.js
@@ -1,10 +1,11 @@
 const request = require("supertest");
 
+// locaal testen en docker testen zijn apart 
 const BASE_URL = process.env.TEST_MODE === "true"
-  ? "http://test-backend:8080"  // ✅ Inside Docker network
+  ? "http://test-backend:8080"  //  Inside Docker network
   : process.env.LOCAL_TEST === "true"
-  ? "http://localhost:8090"     // ✅ Local development
-  : "http://localhost:8081";     // ✅ Jenkins pipeline
+  ? "http://localhost:8090"     //  Local development
+  : "http://localhost:8081";     // Jenkins pipeline
 
 
 describe("Todo API", () => {
